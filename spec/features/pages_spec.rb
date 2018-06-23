91d72ebefaec04bf5e4a864e7049db0282888de9
roberts1000 <roberts@corlewsolutions.com>
2018-06-22 20:48:45 -0400

    Create failure

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "IndexPage" do
-  it "can be viewed" do
+  it "can be viewed", js: true do
     visit root_url
-    expect(page).to have_content("Hello World")
   end
 end
